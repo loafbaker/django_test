4583c40ef575e9b2756d94bc121cabbd567a9118
Jianming Chen <loafbaker@hotmail.com>
2015-09-24 23:48:49 +0800

    Add registration system

--- django_test/views.py
+++ django_test/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.contrib import auth
 from django.core.context_processors import csrf
+from django.contrib.auth.forms import UserCreationForm
 
 def login(request):
     c = {}
@@ -30,6 +31,20 @@ def logout(request):
     auth.logout(request)
     return render_to_response('logout.html')
 
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/accounts/register_success')
 
+    args = {}
+    args.update(csrf(request))
 
+    args['form'] = UserCreationForm()
+    print args
+    return render_to_response('register.html', args)
+
+def register_success(request):
+    return render_to_response('register_success.html')
 
