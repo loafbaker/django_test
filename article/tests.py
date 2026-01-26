c065d9ae47bf3cde907a95a0d72f9c1d13b3f4ee
Jianming Chen <loafbaker@hotmail.com>
2026-01-26 07:20:24 +0000

    Improve test cases for article app

--- article/tests.py
+++ article/tests.py
@@ -5,7 +5,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase, override_settings
 from django.utils import timezone
 
-from .models import Article
+from .models import Article, Comment
 
 TEMP_MEDIA_ROOT = tempfile.mkdtemp()
 
@@ -31,3 +31,24 @@ class ArticleViewsTests(TestCase):
         self.assertEqual(article.likes, 1)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['Location'], f'/articles/get/{article.id}')
+
+    def test_add_comment_creates_comment_and_redirects(self):
+        article = Article.objects.create(
+            title='Test title',
+            body='Test body',
+            pub_date=timezone.now(),
+            thumbnail=SimpleUploadedFile('thumb.jpg', b'test'),
+        )
+
+        response = self.client.post(
+            f'/articles/add_comment/{article.id}/',
+            data={'name': 'Alice', 'body': 'Nice post!'},
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['Location'], f'/articles/get/{article.id}')
+        self.assertEqual(Comment.objects.filter(article=article).count(), 1)
+        comment = Comment.objects.get(article=article)
+        self.assertEqual(comment.name, 'Alice')
+        self.assertEqual(comment.body, 'Nice post!')
+        self.assertIsNotNone(comment.pub_date)
